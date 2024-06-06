75823851f5c10ecc94735ef47e93093b0f3214d2
Rafael Santos <rafaeldias.a@hotmail.com>
2024-06-06 10:10:07 -0300

    Removing unecessary code

--- Challenge.Currency.Api/obj/Debug/net8.0/Challenge.Bravo.Api.AssemblyInfo.cs
+++ Challenge.Currency.Api/obj/Debug/net8.0/Challenge.Bravo.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Challenge.Bravo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85173f742a11413f21dea90d098647fa277e0b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b263ee02bc89e9c8615ba442671f5e771d0d828")]
 [assembly: System.Reflection.AssemblyProductAttribute("Challenge.Bravo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Challenge.Bravo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
